055948d0ebf0a9436469b200b7f1de961cf0dd1d
LucasMorales <lemorales2@gmail.com>
2019-04-04 00:36:01 -0700

    added playability

--- src/Main.java
+++ src/Main.java
@@ -11,30 +11,31 @@ public class Main {
         g.addDirectedEdge("hall", "dungeon");
         g.addUndirectedEdge("hall", "closet");
 
-
+        Graph.Node current = g.getNode("hall");
 
         // game loop code
-/*
 
-        Node currentRoom = root;
         String userResponse = "";
         Scanner in = new Scanner(System.in);
 
         do {
-            System.out.println("You are in the " + currentRoom.getName());
-            System.out.println("You can go to the: " + currentRoom.getNeighborNames());
-            System.out.println("Choose a room to enter and type its name: ");
+            System.out.println("You are in the " + current.getName());
+            System.out.println("What do you want to do?\t [go to <roomname>] [look] [add room <roomname>] [quit]");
             userResponse = in.nextLine();
 
-            Node nextRoom = currentRoom.getNeighbor(userResponse);
-            if (nextRoom == null) {
-                System.out.println("This room cannot be entered or does not exist: " + userResponse);
+            if (userResponse.indexOf("go to") == 0){ // index of in case there's a weirdly named place
+                String roomReq = userResponse.substring(7);
+                if (current.getNeighbor(roomReq) != null) current = g.getNode(roomReq);
+            } else if (userResponse.indexOf("look") == 0) {
+                System.out.println(current.getNeighborNames());
+            } else if (userResponse.indexOf("add room") == 0) {
+                g.addNode(userResponse);
+                g.addDirectedEdge(current.getName(), userResponse);
             } else {
-                currentRoom = nextRoom;
+                System.out.println("What do you want to do?\t [go to <roomname>] [look] [add room <roomname>] [quit]");
             }
 
         } while (!userResponse.equals("quit"));
-*/
 
     }
 }
