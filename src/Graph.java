9f43900642923b7c02945a463441a5980624105f
LucasMorales <lemorales2@gmail.com>
2019-04-04 00:11:17 -0700

    Graph class outlined

---
+++ src/Graph.java
@@ -0,0 +1,73 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Graph {
+    private List<Node> nodes;
+
+    public Graph() {
+
+    }
+
+    public void addNode(String name) {
+    }
+
+    public void addDirectedEdge(String node1, String node2) {
+        Node n1 = getNode(node1);
+        Node n2 = getNode(node2);
+        n1.addNeighbor(n2);
+    }
+
+    public void addUndirectedEdge(String node1, String node2) {
+        Node n1 = getNode(node1);
+        Node n2 = getNode(node2);
+        n1.addNeighbor(n2);
+        n2.addNeighbor(n1);
+
+    }
+
+    public Node getNode(String name) {
+        return null;
+    }
+
+
+    public class Node {
+        private String name;
+        private ArrayList<Node> neighbors;
+
+        private Node(String name) {
+            neighbors = new ArrayList<Node>();
+            this.name = name;
+        }
+
+        private void addNeighbor(Node n) {
+            neighbors.add(n);
+        }
+
+        public String getNeighborNames() {
+            String names = "";
+            for (Node n : neighbors) {
+                names += n.getName();
+                names += "\t";
+            }
+            return names;
+        }
+
+        public Node getNeighbor(String name) {
+            int indexOfN = -1;
+            for (int i = 0; i < neighbors.size(); i++) {
+                if (neighbors.get(i).getName().equals(name)) indexOfN = i;
+            }
+            if (indexOfN == -1) {
+                System.out.println("no neighbor of this name: " + name);
+                return null;
+            }
+            return neighbors.get(indexOfN);
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+}
