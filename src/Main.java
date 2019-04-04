116ac514eb079f02fa7c8f4e4c93dab2dd3b4b50
LucasMorales <lemorales2@gmail.com>
2019-04-04 10:29:13 -0700

    game loop functioning

--- src/Main.java
+++ src/Main.java
@@ -20,20 +20,27 @@ public class Main {
 
         do {
             System.out.println("You are in the " + current.getName() + ".");
-            System.out.println("What do you want to do?\t [go to <roomname>] [look] [add room <roomname>] [quit]");
+            System.out.println("What do you want to do? Your options are:");
+            System.out.println("[go to <roomname>]\t[look]\t[add room <roomname>]\t[quit]");
             userResponse = in.nextLine();
+            System.out.println();
 
             if (userResponse.indexOf("go to") == 0) { // indexOf in case there's a weirdly named place
                 String roomReq = userResponse.substring(6);
-                if (current.getNeighbor(roomReq) != null) current = g.getNode(roomReq);
+
+                if (current.hasNeighbor(roomReq))
+                    current = g.getNode(roomReq);
+                else
+                    System.out.println("this room does not exist: " + roomReq);
 
             } else if (userResponse.indexOf("look") == 0) {
-                System.out.println("Your options are: " + current.getNeighborNames() + "\n");
+                System.out.println("Your room options are: " + current.getNeighborNames() + "\n");
 
             } else if (userResponse.indexOf("add room") == 0) {
-                g.addNode(userResponse);
-                g.addDirectedEdge(current.getName(), userResponse);
-                System.out.println(userResponse + " has been added as a room.\n");
+                String newRoomReq = userResponse.substring(9);
+                g.addNode(newRoomReq);
+                g.addDirectedEdge(current.getName(), newRoomReq);
+                System.out.println(newRoomReq + " has been added as a room.\n");
 
             } else {
                 System.out.println("What do you want to do?\t [go to <roomname>] [look] [add room <roomname>] [quit]");
